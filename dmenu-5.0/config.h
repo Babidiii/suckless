a57297915a980a782cfcc392a59d730add9f95f8
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-03-10 23:54:52 +0100

    center change

--- dmenu-5.0/config.h
+++ dmenu-5.0/config.h
@@ -2,11 +2,15 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 800;                    /* minimum width when centered */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10",
   "JoyPixels:pixelsize=10"
 };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
@@ -14,6 +18,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#b94949" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 static unsigned int columns    = 0;

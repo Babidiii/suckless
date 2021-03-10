3029bfbbafa5f74a551b36421c14cb18339d486e
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-03-10 23:33:25 +0100

    patches added: lineheight, grid, numbers

--- dmenu-5.0/config.h
+++ dmenu-5.0/config.h
@@ -11,11 +11,16 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#b94949" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
+
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 24;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
